5e0e14b94f8cbb53cb19d3c61f1c01d7e4a08e24
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2022-12-13 17:31:28 +0100

    :sparkles: small changes

--- include/fiction/technology/sidb_charge_state.hpp
+++ include/fiction/technology/sidb_charge_state.hpp
@@ -15,5 +15,33 @@ enum class sidb_charge_state
     NEUTRAL,
     NEGATIVE
 };
+/**
+ * Convert the charge state into int (-1,0,1)
+ *
+ * @param cs sidb charge state
+ * @return int representing the SiDB's charge state
+ */
+[[nodiscard]] int transform_to_sign(const sidb_charge_state& cs) noexcept
+{
+    if (cs == sidb_charge_state::POSITIVE)
+    {
+        return +1;
+    }
+    else if (cs == sidb_charge_state::NEGATIVE)
+    {
+        return -1;
+    }
+
+    else if (cs == sidb_charge_state::NEUTRAL)
+    {
+        return 0;
+    }
+
+    else
+    {
+        return 0;
+    }
+
+}
 
 #endif  // FICTION_SIDB_CHARGE_STATE_HPP
