ad11149cd87b8683e2a0757a7ffd801dbb1d8d54
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2022-12-13 09:55:54 +0100

    :sparkles: Distance matrix where all inter-distances are stored, test included

--- include/fiction/algorithms/simulation_sidb/distance_matrix.hpp
+++ include/fiction/algorithms/simulation_sidb/distance_matrix.hpp
@@ -4,32 +4,47 @@
 // Created by Jan Drewniok on 07.12.22.
 //
 
+#include "fiction/algorithms/path_finding/distance.hpp"
+#include "fiction/algorithms/simulation_sidb/constants.hpp"
+#include "fiction/layouts/coordinates.hpp"
+#include "fiction/technology/charge_distribution_surface.hpp"
 #include "fiction/traits.hpp"
 #include "fiction/types.hpp"
-#include "fiction/technology/charge_distribution_surface.hpp"
-#include "fiction/layouts/coordinates.hpp"
-#include "fiction/algorithms/simulation_sidb/constants.hpp"
-#include "fiction/algorithms/path_finding/distance.hpp"
 
 namespace fiction
 {
-
+/**
+ * The distance matrix is an unordered map with pairs of cells as key and the corresponding euclidean distance as value.
+ */
 template <typename Lyt, typename Dist = double>
 using distance_matrix = std::unordered_map<std::pair<const cell<Lyt>, const cell<Lyt>>, Dist>;
 
+/**
+ * The distance matrix stores all possible euclidean distances between two SiDBs. Also the euclidean distance between two identical SiDBs is stored as "0.0" distance.
+ *  \f$ D = \sqrt{(x_1 - x_2)^2 + (y_1 - y_2)^2} \f$
+ *
+ * @tparam Lyt Coordinate layout type (SiQAD coordinates are required).
+ * @tparam Dist Floating-point type for the distance.
+ * @param lyt Layout.
+ * @return Distance matrix
+ */
 template <typename Lyt, typename Dist = double>
-distance_matrix<Lyt,Dist> distance_SiDBs(const Lyt& lyt)
+distance_matrix<Lyt, Dist> distance_SiDBs(const Lyt& lyt)
 {
     distance_matrix<Lyt, Dist> distance_values{};
     lyt.foreach_cell(
         [&distance_values, lyt](const auto& c1)
         {
-            lyt.foreach_cell([&distance_values, c1, lyt](const auto& c2)
-                             { distance_values.insert(std::pair(c1, c2), distance_SiDB_pair<Lyt>(lyt, c1, c2)); });
+            lyt.foreach_cell(
+                [&distance_values, c1, lyt](const auto& c2) {
+                    distance_values.insert(
+                        std::make_pair(std::make_pair(c1, c2), distance_SiDB_pair<Lyt>(lyt, c1, c2)));
+                });
         });
+
     return distance_values;
 };
 
-} // namespace fiction
+}  // namespace fiction
 
 #endif  // FICTION_DISTANCE_MATRIX_HPP
--- test/algorithms/simulation_sidb/distance_matrix.cpp
+++ test/algorithms/simulation_sidb/distance_matrix.cpp
@@ -15,21 +15,29 @@
 using namespace fiction;
 
 TEMPLATE_TEST_CASE(
-    "SiDB surface traits and construction", "[distance-matrix]",
-    (cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>))
-//    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<offset::ucoord_t, odd_row_hex>>>),
-//    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<offset::ucoord_t, even_row_hex>>>),
-//    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<offset::ucoord_t, odd_column_hex>>>),
-//    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<offset::ucoord_t, even_column_hex>>>))
+    "Distance Matrix calculation", "[distance-matrix]",
+    (cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>),
+    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, odd_row_hex>>>),
+    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, even_row_hex>>>),
+    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, odd_column_hex>>>),
+    (cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, even_column_hex>>>))
 {
     TestType lyt{{3,3}};
 
     charge_distribution_surface charge_layout{lyt};
 
     charge_layout.assign_cell_type({0, 0, 0}, TestType::cell_type::NORMAL);
-    charge_layout.assign_cell_type({1, 0, 1}, TestType::cell_type::NORMAL);
+    charge_layout.assign_cell_type({1, 0, 0}, TestType::cell_type::NORMAL);
+    charge_layout.assign_cell_type({1, 1, 1}, TestType::cell_type::NORMAL);
 
-    auto distance = distance_SiDBs<charge_distribution_surface>(charge_layout);
+    auto distance = distance_SiDBs(charge_layout);
+    CHECK(distance.at({{0, 0, 0}, {0, 0, 0}}) == 0.0);
+    CHECK(distance.at({{0, 0, 0}, {1, 0, 0}}) == simulation_parameter{}.lat_a);
+    CHECK(distance.at({{1, 0, 0}, {0, 0, 0}}) == simulation_parameter{}.lat_a);
+    CHECK(distance.at({{1, 0, 0}, {1, 0, 0}}) == 0.0);
+    CHECK(distance.at({{0, 0, 0}, {1, 1, 1}}) == std::hypot(simulation_parameter{}.lat_a,simulation_parameter{}.lat_b + simulation_parameter{}.lat_c));
+    CHECK(distance.at({{1, 1, 1}, {1, 1, 1}}) == 0);
+
 
 }
 
