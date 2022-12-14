87a5a40e8a8111a039fd8f61f488245c069f2175
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2022-12-14 08:19:37 +0100

    :sparkles: changed member names

--- include/fiction/algorithms/simulation_sidb/constants.hpp
+++ include/fiction/algorithms/simulation_sidb/constants.hpp
@@ -12,11 +12,11 @@ namespace constants
     /**
      * vacuum permittivity
      */
-    const double epsilon           = 8.854 * 1E-12;
+    const double EPSILON           = 8.854 * 1E-12;
     /**
      * electric charge
      */
-    const double e                 = 1.602 * 1E-19;
+    const double ELECTRIC_CHARGE                 = 1.602 * 1E-19;
     /**
      * stability threashold
      */
